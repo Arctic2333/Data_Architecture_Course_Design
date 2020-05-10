461c939b2e74661ec841b2afb5e2583de2c51a62
苏淞丰 <50102735+Arctic2333@users.noreply.github.com>
2020-05-10 15:00:43 +0800

    Add Browsing Function

--- Course_Design/src/main/java/view/Ui.java
+++ Course_Design/src/main/java/view/Ui.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -16,19 +17,26 @@ import java.io.IOException;
  * @date 2020/5/916:59
  */
 public class Ui {
-    private JFrame frame = new JFrame("拯救学委2.0");  // 窗口
+    private JFrame frame = new JFrame("拯救学委3.0");  // 窗口
     private JTextField text1 = new JTextField();  // 输入的是目标文件夹
     private JTextField text2 = new JTextField();  // 输入的是关键字
     private JLabel label1 = new JLabel("移动或解压目标文件夹:");  // 标签1
     private JLabel label2 = new JLabel("查询或移动关键字:");  // 标签2
     private JScrollPane scrollPane_1 = new JScrollPane();  // 滚动条
     private static JTextArea area = new JTextArea(10, 40);  // 文本域
-    private JButton button = new JButton("开始执行");  // 按钮
+    private JButton btn_move_start = new JButton("开始执行");  // move功能按钮
+    private JButton btn_browse = new JButton("浏览");  // 浏览文件夹按钮
+
     ActionHandle handle = new ActionHandle();  // 事件监听
+    JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));  // 设置浏览为当前路径下的文件夹
+    File chose_file = null;  // 浏览选择的文件
 
     public Ui() {
+        //设置只可选目录
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         //添加监听事件
-        button.addActionListener(handle);
+        btn_move_start.addActionListener(handle);
+        btn_browse.addActionListener(handle);
         // 设置窗口位于屏幕中间
         int sWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
         int sHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
@@ -66,9 +74,13 @@ public class Ui {
         scrollPane_1.setViewportView(area);  // 向文件域中添加滚动条
 
 
-        button.setSize(120, 30);
-        button.setBounds(midX, midY, button.getWidth(), button.getHeight());
-        frame.add(button);
+        btn_move_start.setSize(120, 30);
+        btn_move_start.setBounds(midX, midY, btn_move_start.getWidth(), btn_move_start.getHeight());
+        frame.add(btn_move_start);
+
+        btn_browse.setSize(60, 25);
+        btn_browse.setBounds(midX + 260, midY - 35, btn_browse.getWidth(), btn_browse.getHeight());
+        frame.add(btn_browse);
 
         frame.setVisible(true);
     }
@@ -83,7 +95,7 @@ public class Ui {
     class ActionHandle implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == button) {
+            if (e.getSource() == btn_move_start) {  // move功能开始执行
                 try {
                     Move_Files.move_Files(text1.getText(), text2.getText());  // 将两个文本框中内容当作参数传入
                     System.out.println("GUI.out:移动成功");  // 终端测试可观，与control层输出区分开
@@ -91,6 +103,11 @@ public class Ui {
                     ioException.printStackTrace();
                     System.out.println("GUI.out:移动失败");  // 终端测试可观，与control层输出区分开
                 }
+            }
+            if (e.getSource() == btn_browse) {  // 选择文件夹
+                fileChooser.showOpenDialog(frame);  // 显示filechooser
+                chose_file = fileChooser.getSelectedFile();  // 被选中的文件夹
+                text1.setText(chose_file.getName());  // 将被选择的文件夹名字加入text1
             }
         }
     }
