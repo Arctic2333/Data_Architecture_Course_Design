e29af406b4f844738ff5de3b8f508095668d31da
苏淞丰 <50102735+Arctic2333@users.noreply.github.com>
2020-05-10 10:29:15 +0800

    SSC 2.0 VIEW and CONTROL Separation End

--- Course_Design/src/main/java/control/Move_Files.java
+++ Course_Design/src/main/java/control/Move_Files.java
@@ -1,40 +1,54 @@
 package control;
 
+import com.sun.org.glassfish.gmbal.Description;
 import view.Ui;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Susongfeng
  * @Title: Move_Files
  * @Package: control
  * @Description: Move files with keywords from the current directory to the specified folder
- * @date 2020/5/917:13
+ * @date 2020/5/9 17:13
  */
 public class Move_Files {
     @SuppressWarnings("all")
-    public static void move_Files(String str1, String str2) {
+    /**
+     * @Description: 指定到指定目录和移动文件名中带有关键字的文件
+     * @Param: [str1, str2]
+     * @return: void
+     * @Date: 2020/5/9
+     */
+    public static void move_Files(String str1, String str2) throws IOException {  // str1：目标文件夹  str2：文件名关键字
         int total = 0;
         File folder = new File(System.getProperty("user.dir"));  // 获取当期路径下的文件和文件夹
-        File[] files = folder.listFiles();  // 筛选出文件成数组
-        for (int i = 0; i < files.length; i++) {  // 遍历文件通过特定字进行分类  采用renameto  永久重命名移动
-            if (files[i].isFile()) {
-                String name = files[i].getName();
-                if (name.indexOf(str2) != -1) {
+        File[] files = folder.listFiles();  // 将获取的文件及文件夹列表导入数组
+        for (int i = 0; i < files.length; i++) {  // 遍历文件
+            if (files[i].isFile()) {  // 判断是否为文件
+                String name = files[i].getName();  // 获取文件名字
+                if (name.indexOf(str2) != -1) {  // 判断是否带有关键字
+                    // 采用方法renameto()永久重命名移动
                     files[i].renameTo(new File(System.getProperty("user.dir") + "/" + str1 + "/" + files[i].getName()));
-                    Ui.setArea(name);
-                    total++;
+                    Ui.setArea(name);  // 输出移动成功的文件名到文件域
+                    total++;  // 移动成功文件数加一
                 }
             }
         }
-        Ui.setArea(total);
-        System.out.println("移动成功");
+        Ui.setArea(total);  // 输出移动成功的文件数
+        System.out.println("*********************移动成功**********************");  // 终端测试可观
+        System.out.println("成功移动：" + total + "个");  // 终端测试可观
+        System.out.println("当前工作路径："+System.getProperty("user.dir"));  // 终端测试可观 当前路径
+        return;
     }
 
-    public static void main(String[] args) {
-        move_Files("test", "test");
+    public static void main(String[] args) {  // 功能1，逻辑功能测试
+        try {
+            move_Files("test", "test");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- Course_Design/src/main/java/view/Ui.java
+++ Course_Design/src/main/java/view/Ui.java
@@ -1,7 +1,12 @@
 package view;
 
+import control.Move_Files;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * @author Susongfeng
@@ -11,51 +16,54 @@ import java.awt.*;
  * @date 2020/5/916:59
  */
 public class Ui {
-    private JFrame frame = new JFrame("拯救学委2.0");
-    private JTextField text1 = new JTextField();
-    private JTextField text2 = new JTextField();
-    private JLabel label1 = new JLabel("目标文件夹:");
-    private JLabel label2 = new JLabel("关键字:");
-    private JScrollPane scrollPane_1 = new JScrollPane();
-    private static JTextArea area = new JTextArea(10, 40);
-    private JButton button = new JButton("开始执行");
+    private JFrame frame = new JFrame("拯救学委2.0");  // 窗口
+    private JTextField text1 = new JTextField();  // 输入的是目标文件夹
+    private JTextField text2 = new JTextField();  // 输入的是关键字
+    private JLabel label1 = new JLabel("移动或解压目标文件夹:");  // 标签1
+    private JLabel label2 = new JLabel("查询或移动关键字:");  // 标签2
+    private JScrollPane scrollPane_1 = new JScrollPane();  // 滚动条
+    private static JTextArea area = new JTextArea(10, 40);  // 文本域
+    private JButton button = new JButton("开始执行");  // 按钮
+    ActionHandle handle = new ActionHandle();  // 事件监听
 
     public Ui() {
+        //添加监听事件
+        button.addActionListener(handle);
         // 设置窗口位于屏幕中间
         int sWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
         int sHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
         int winX = (sWidth - 500) / 2;
         int winY = (sHeight - 300) / 2;
-        frame.setBounds(winX + 50, winY + 50, 500, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
+        frame.setBounds(winX + 50, winY + 50, 700, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // 退出、最小化、关闭
+        frame.setLayout(null);  // 空白布局
 
-        int midX = 500 / 2 - 120 / 2;
-        int midY = 300 / 2 - 30 / 2;
-
-
-        text1.setBounds(midX, midY - 35, 120, 30);
-        frame.add(text1);
+        int midX = 700 / 2 - 120 / 2;  // 窗口中间位置
+        int midY = 400 / 2 - 30 / 2;
 
 
-        text2.setBounds(midX, midY - 75, 120, 30);
+        text1.setBounds(midX + 50, midY - 35, 200, 25);
+        frame.add(text1);  // 向窗口添加text1
+
+
+        text2.setBounds(midX + 50, midY - 75, 200, 25);
         frame.add(text2);
 
 
-        label1.setBounds(midX - 80, midY - 35, 70, 30);
+        label1.setBounds(midX - 100, midY - 35, 140, 30);
         frame.add(label1);
 
 
-        label2.setBounds(midX - 80, midY - 75, 70, 30);
+        label2.setBounds(midX - 80, midY - 75, 110, 30);
         frame.add(label2);
 
 
         scrollPane_1.setBounds(0, 130, 175, 120);
-        scrollPane_1.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane_1.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);  // 默认显示滚动条
         frame.getContentPane().add(scrollPane_1);
 
         area.setBounds(0, 130, 175, 120);
-        scrollPane_1.setViewportView(area);
+        scrollPane_1.setViewportView(area);  // 向文件域中添加滚动条
 
 
         button.setSize(120, 30);
@@ -65,15 +73,37 @@ public class Ui {
         frame.setVisible(true);
     }
 
-    public static void setArea(String text) {
+    /**
+     * @author Susongfeng
+     * @Title: ActionHandle
+     * @Package view
+     * @Description: ActionListener Class
+     * @date 2020/5/10 00:58
+     */
+    class ActionHandle implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == button) {
+                try {
+                    Move_Files.move_Files(text1.getText(), text2.getText());  // 将两个文本框中内容当作参数传入
+                    System.out.println("GUI.out:移动成功");  // 终端测试可观，与control层输出区分开
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                    System.out.println("GUI.out:移动失败");  // 终端测试可观，与control层输出区分开
+                }
+            }
+        }
+    }
+
+    public static void setArea(String text) {  // 输出内容到文本域
         area.append(text + '\n');
     }
 
-    public static void setArea(int n) {
+    public static void setArea(int n) {  // 输出内容到文本域
         area.append("成功移动文件： " + n + " 个\n");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // GUI测试
         new Ui();
     }
 }
